dd39826614027337a44b0f0f1bf053a1ad4e6ac0
Panos Kountanis <panosktn@gmail.com>
2012-01-28 00:37:47 +0200

    adding address extractor

--- test/validate-test.js
+++ test/validate-test.js
@@ -2,9 +2,10 @@ var vows = require('vows'),
     assert = require('assert');
 
 var	Validate = require('./../lib/validate').Validate;
+var Extract = require('./../lib/validate').Extract;
 
 vows.describe('OSC Validate:').addBatch({
-	'OSC Address Symbol': {
+	'OSC Address Name': {
 		topic: null,
 		'should not contain white spaces': function (topic) {
 			assert.equal(false, Validate.addressSymbol('address with spaces'));
@@ -35,6 +36,12 @@ vows.describe('OSC Validate:').addBatch({
 		},
 		'should not contain }': function (topic) {
 			assert.equal(false, Validate.addressSymbol('addresswith}'));
+		}
+	},
+	'Extract': {
+		topic: null,
+		'should extract address components': function (topic) {
+			assert.deepEqual(['an', 'osc', 'address'], Extract.address('/an/osc/address'));
 		}
 	}
 }).export(module);
