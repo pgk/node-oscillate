74e18bf2d435cc7cc5adf424ebaf9bb4093259ec
Panos Kountanis <panosktn@gmail.com>
2012-02-13 23:36:38 +0200

    refactoring server and client

--- examples/ping.js
+++ examples/ping.js
@@ -1,6 +1,6 @@
 var Client = require('./../oscillate').oscillate.Client;
 
-var c = new Client(7374, 15432, '127.0.0.1', true);
+var c = new Client(17374, 7374, '127.0.0.1', true);
 
 setInterval(function() {
 	c.send('/foo/bar', 'hello from osc client');
--- examples/simple-server.js
+++ examples/simple-server.js
@@ -1,6 +1,6 @@
 var Server = require('./../oscillate').oscillate.Server;
 
-var s = new Server(7377, '127.0.0.1');
+var s = new Server(7377, 7374, '127.0.0.1', true);
 
 s.on('/foo/bar', function (arr) {
 	console.log('bar: ' + arr[1]);
